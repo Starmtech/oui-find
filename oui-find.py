8659cf76acb9e80f0b9410101a8806406036ad35
devkort <jeromet9208@gmail.com>
2016-12-04 18:19:06 +0100

    Update oui-find.py

--- oui-find.py
+++ oui-find.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-###################################
-#           OuiFInd               #
-#      langage : Python 2.7       # 
-#         date : 13/11/16         #
-#          version : 1.0          #
-#        auteur : devkort         #
-###################################
+#This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 only.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  
+#    USA
+#           OuiFInd               
+#      langage : Python 2.7       
+#         date : 13/11/16         
+#          version : 1.0          
+#        auteur : devkort         
+
 
 import sqlite3
 import os
